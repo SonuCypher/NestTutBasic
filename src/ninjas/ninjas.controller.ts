eb252aece61cb8c65199c63ff714fc11839da577
Cypher890 <abdulsuhaib07@gmail.com>
2023-10-30 21:06:02 +0530

    nest controller props

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('ninjas')
 export class NinjasController {
@@ -8,7 +8,25 @@ export class NinjasController {
     }
 
     @Get(':id')
-    getOneNinja(){
-        return {};
+    getOneNinja(@Param('id') id: string){
+        return {
+            id,
+        };
     }
+
+    @Post()
+    createNinja(){
+        return{}
+    }
+
+    @Put(':id')
+    updateNinja(@Param('id') id: string){
+        return{}
+    }
+
+    @Delete('id')
+    removeNinja(){
+        return{}
+    }
+
 }
